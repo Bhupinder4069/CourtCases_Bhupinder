f02b98eed72dd5cfaeb74684c57b882851c2c75f
bhupinder1992011@gmail.com <bhupinder1992011@gmail.com>
2021-11-18 12:56:33 +0300

    Add project files.

---
+++ CourtCases_Bhupinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourtCases_Bhupinder.MvcApplication" Language="C#" %>
---
+++ CourtCases_Bhupinder/Migrations/202110112213548_iniiit1.cs
@@ -0,0 +1,16 @@
+namespace CourtCases_Bhupinder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class iniiit1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
